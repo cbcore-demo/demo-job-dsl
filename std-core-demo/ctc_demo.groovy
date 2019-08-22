7d29c479a00671d4a0d1bf188d90819bfc62c310
philcgithub <phil.cherry@gmail.com>
2019-08-22 12:05:58 +0100

    Changed quotes to apostrophes

--- std-core-demo/ctc_demo.groovy
+++ std-core-demo/ctc_demo.groovy
@@ -5,31 +5,32 @@ folder('Cross Team Collaboration') {
 pipelineJob("Cross Team Collaboration/CTC Build job") {
 	definition {
 		cps {
-"""pipeline {
-    agent { label 'master' } 
-    stages {
-        stage('Build') { 
-            steps {
-                echo 'This is the build stage'
-            }
-        }
-        stage('Test') { 
-            steps {
-                echo 'This is the test stage'
-            }
-        }
-        stage('Deploy') { 
-            steps {
-                echo 'This is the deploy stage'
-            }
-        }
-        stage('Trigger next job') {
-            steps {
-                echo 'sending triggerjob'
-                publishEvent jsonEvent('{"eventName":"triggerjob"}')
-            }
-        }
-    }
-}"""		}
+		'''pipeline {
+		    agent { label 'master' } 
+		    stages {
+			stage('Build') { 
+			    steps {
+				echo 'This is the build stage'
+			    }
+			}
+			stage('Test') { 
+			    steps {
+				echo 'This is the test stage'
+			    }
+			}
+			stage('Deploy') { 
+			    steps {
+				echo 'This is the deploy stage'
+			    }
+			}
+			stage('Trigger next job') {
+			    steps {
+				echo 'sending triggerjob'
+				publishEvent jsonEvent('{"eventName":"triggerjob"}')
+			    }
+			}
+		    }
+		}'''
+	   }
 	}
 }
